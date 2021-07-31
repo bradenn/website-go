6f95de56f66a737263b55df915032c7b48537b0c
bradenn <braden@bradencnicholson.com>
2021-07-31 16:03:10 -0700

    typo hotfix

--- server/router.go
+++ server/router.go
@@ -12,17 +12,12 @@ func NewRouter() *gin.Engine {
 	r.StaticFile("/favicon.ico", "./static/favicon.ico")
 
 	/* Heartbeat */
-	r.GET("/status", func(c *gin.Context) {
-		c.String(http.StatusOK, "ok")
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "Pong!")
 	})
 
 	/* Homepage */
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "home.tmpl", gin.H{"title": "bradenNicholson", "desc": "Software Engineer In California"})
-	})
-
-	/* Projects */
-	r.GET("/*", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "home.tmpl", gin.H{"title": "bradenNicholson", "desc": "Software Engineer In California"})
 	})
 
